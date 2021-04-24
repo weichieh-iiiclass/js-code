8c4c612858a9127351a74885ce5e4e4bf3816f45
weichieh123 <weichiehhuang@gapp.nthu.edu.tw>
2021-04-24 21:46:08 +0800

    complete 17.postcode

---
+++ public/17.郵遞區號/index.js
@@ -0,0 +1,36 @@
+const country = document.getElementById('country')
+const township = document.getElementById('township')
+const postcode = document.getElementById('postcode')
+
+let countryIndex = -1
+let townshipIndex = -1
+
+//display country options
+const defaultOption = `<option value"-1">請選擇</option>`
+country.innerHTML =
+  defaultOption + countries.map((v, i) => `<option value="${i}">${v}</option>`)
+
+// select country
+country.addEventListener('change', function (e) {
+  countryIndex = +e.target.value
+  // countryIndex = +country.value
+  console.log(countryIndex)
+
+  //display township options
+  township.innerHTML =
+    defaultOption +
+    townships[countryIndex].map((v, i) => `<option value="${i}">${v}</option>`)
+})
+
+// select township
+township.addEventListener('change', function (e) {
+  townshipIndex = +e.target.value
+  console.log(townshipIndex)
+
+  //display postcode
+  postcode.innerHTML = postcodes[countryIndex][townshipIndex]
+})
+
+//   for (let i = 0; i < townships[countryIndex][].length; i++) {
+//     township.innerHTML += `<option value"${i}">${townships[countryIndex][i]}</option>`
+//   }
