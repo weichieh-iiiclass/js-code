517801c6d51e78687fbef1d02a75be444ed09e82
weichieh123 <weichiehhuang@gapp.nthu.edu.tw>
2021-04-27 14:04:39 +0800

    0427 delete comment

--- public/17.郵遞區號/index.js
+++ public/17.郵遞區號/index.js
@@ -31,6 +31,3 @@ township.addEventListener('change', function (e) {
   postcode.innerHTML = postcodes[countryIndex][townshipIndex]
 })
 
-//   for (let i = 0; i < townships[countryIndex][].length; i++) {
-//     township.innerHTML += `<option value"${i}">${townships[countryIndex][i]}</option>`
-//   }
